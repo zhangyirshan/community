5666df3c8e8032d9228ca8622be89aefc91bf11d
JavaMatthew <941076795@qq.com>
2019-09-08 08:09:49 +0800

    将github的变量写入配置文件

--- src/main/java/com/matthew/community/controller/AuthorizeController.java
+++ src/main/java/com/matthew/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import com.matthew.community.dto.AccessTokenDTO;
 import com.matthew.community.dto.GithubUser;
 import com.matthew.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,14 +21,21 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("d9b6864669ab3015c123");
-        accessTokenDTO.setClient_secret("53897fc2eee23ecd5762f2e1bd1b1b85b07fdc72");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8888/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
