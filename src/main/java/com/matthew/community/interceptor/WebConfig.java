1437e259aed247ebfb04aa9fb0ac7cf1f1be96b1
JavaMatthew <941076795@qq.com>
2019-10-18 11:45:05 +0800

    使用MarkDown替代文本框

--- src/main/java/com/matthew/community/interceptor/WebConfig.java
+++ src/main/java/com/matthew/community/interceptor/WebConfig.java
@@ -21,6 +21,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(sessionInterceptor).addPathPatterns("/**");
+        registry.addInterceptor(sessionInterceptor).addPathPatterns("/**").excludePathPatterns("/css/**","/fonts/**","/images/**","/js/**");
     }
 }
